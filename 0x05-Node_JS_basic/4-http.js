304035fe15275f9cf4ce1345dc7da709c6605878
Ahmed-Nageb <ahmednageb61.an@gmail.com>
2025-01-21 11:33:53 +0200

    all

--- 0x05-Node_JS_basic/4-http.js
+++ 0x05-Node_JS_basic/4-http.js
@@ -6,7 +6,7 @@ const app = http.createServer();
 
 
 app.on('request', (_, res) => {
-  const responseText = 'Hello Holberton School!';
+  const responseText = 'Hello Alx!';
 
   res.setHeader('Content-Type', 'text/plain');
   res.setHeader('Content-Length', responseText.length);
